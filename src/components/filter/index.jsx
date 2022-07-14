f47acac9f60b43d70d87568dc7048ebab7bdf849
AzureSphere <dustin.jason.lee@gmail.com>
2022-07-14 23:47:39 +0200

    Added a filter component to show items in the database for easier search

---
+++ src/components/filter/index.jsx
@@ -0,0 +1,101 @@
+import * as React from "react";
+import "./style.css";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Checkbox from "@mui/material/Checkbox";
+import IconButton from "@mui/material/IconButton";
+
+export default function Filter() {
+  const [checked, setChecked] = React.useState([0]);
+
+  const handleToggle = value => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  return (
+    <div className="container">
+      <List>
+        <h4>Objects</h4>
+        {["AWSZone", "AWSRegion", "Subnet", "AWSVpc", "SecurityGroup"].map(
+          value => {
+            const labelId = `checkbox-list-label-${value}`;
+
+            return (
+              <ListItem key={value} disablePadding>
+                <ListItemButton
+                  role={undefined}
+                  onClick={handleToggle(value)}
+                  dense
+                >
+                  <ListItemIcon>
+                    <Checkbox
+                      edge="start"
+                      checked={checked.indexOf(value) !== -1}
+                      tabIndex={-1}
+                      disableRipple
+                      inputProps={{ "aria-labelledby": labelId }}
+                    />
+                  </ListItemIcon>
+                  <ListItemText id={labelId} primary={`${value}`} />
+                </ListItemButton>
+              </ListItem>
+            );
+          }
+        )}
+        <h4>Relationships</h4>
+        {[
+          "geo",
+          "Peering",
+          "VpcOwner",
+          "ownTgw",
+          "PortRangeSG",
+          "geo",
+          "Peering",
+          "VpcOwner",
+          "ownTgw",
+          "PortRangeSG",
+          "geo",
+          "Peering",
+          "VpcOwner",
+          "ownTgw",
+          "PortRangeSG",
+        ].map(value => {
+          const labelId = `checkbox-list-label-${value}`;
+
+          return (
+            <ListItem key={value} disablePadding>
+              <ListItemButton
+                role={undefined}
+                onClick={handleToggle(value)}
+                dense
+              >
+                <ListItemIcon>
+                  <Checkbox
+                    edge="start"
+                    checked={checked.indexOf(value) !== -1}
+                    tabIndex={-1}
+                    disableRipple
+                    inputProps={{ "aria-labelledby": labelId }}
+                  />
+                </ListItemIcon>
+                <ListItemText id={labelId} primary={`${value}`} />
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
+      </List>
+    </div>
+  );
+}
