e3bc95b57002801454b9ad3ffdd888039137537b
AzureSphere <dustin.jason.lee@gmail.com>
2022-07-15 23:10:40 +0200

    Added a neo4j provider in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
 import App from "./components/App";
 import "./typography.css";
+//neo4j stuff
+import { Neo4jProvider, createDriver } from "use-neo4j";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const driver = createDriver("bolt", "localhost", 7687, "neo4j", "pass");
+
+ReactDOM.render(
+  <Neo4jProvider driver={driver}>
+    <App />
+  </Neo4jProvider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
