f6aaa06acf4b7e3927d13461e0484cba7564ed76
AzureSphere <dustin.jason.lee@gmail.com>
2022-07-22 11:58:29 +0200

    Added the cisco logo on the top left corner

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FilterIcon from "./filterIcon";
 import { ResponsiveNeoGraph } from "./NeoGraph";
+import Logo from "../images/Cisco_Logo.png";
 
 const NEO4J_URI = "bolt://localhost:7687";
 const NEO4J_USER = "neo4j";
@@ -12,6 +13,12 @@ const App = () => {
       className="App"
       style={{ position: "flex", height: "100vh", fontFamily: "Quicksand" }}
     >
+      <img
+        src={Logo}
+        sizes="4"
+        alt="Cisco Systems, Inc. logo"
+        style={{ position: "fixed", height: "2rem", top: "2rem", left: "2rem" }}
+      />
       <ResponsiveNeoGraph
         containerId={"id0"}
         neo4jUri={NEO4J_URI}
